c60c8d419a4bb2e3f77bfcedac88ffe811b27922
RitikaTiwari1911 <spk2ritika1911@gmail.com>
2021-04-25 22:13:18 +0530

    ADDED:Ability to create linked list by adding values from last

--- src/test/java/com/linkedlist/MyLinkedListTest.java
+++ src/test/java/com/linkedlist/MyLinkedListTest.java
@@ -21,4 +21,22 @@ public class MyLinkedListTest {
                          myLinkedList.tail.equals(myFirstNode);
         Assert.assertTrue(result);
     }
+
+    @Test
+    public void given3Numbers_WhenAddedTo_LinkedList_ShouldBeAdded_AtLast(){
+        MyNode<Integer>myFirstNode=new MyNode<>(56);
+        MyNode<Integer>mySecondNode=new MyNode<>(30);
+        MyNode<Integer>myThirdNode=new MyNode<>(70);
+
+        MyLinkedList myLinkedList=new MyLinkedList();
+
+        myLinkedList.add(myFirstNode);
+        myLinkedList.add(mySecondNode);
+        myLinkedList.add(myThirdNode);
+
+        boolean result = myLinkedList.head.equals(myFirstNode)&&
+                myLinkedList.head.getNext().equals(mySecondNode)&&
+                myLinkedList.tail.equals(myThirdNode);
+        Assert.assertTrue(result);
+    }
 }
