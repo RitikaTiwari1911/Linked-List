2918407d44cb695564c420178502764b213e6b08
RitikaTiwari1911 <spk2ritika1911@gmail.com>
2021-04-25 19:39:07 +0530

    ADDED: Created a linked list

--- src/main/java/com/linkedlist/MyNode.java
+++ src/main/java/com/linkedlist/MyNode.java
@@ -1,7 +1,24 @@
 package com.linkedlist;
 
-public class MyNode {
-    public static void main(String args[]) {
+public class MyNode<K>{
+    private K key;
+    private MyNode next;
+
+    public MyNode(K key){
+        this.key=key;
+        this.next=null;
+    }
+
+    public MyNode getNext() {
+        return next;
+    }
+
+    public void setNext(MyNode next) {
+        this.next = next;
+    }
+
+    public static void main(String args []){
         System.out.println("Welcome to the implementation of Linked List !");
     }
+
 }
